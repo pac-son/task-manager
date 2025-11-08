b9256665d320f9439e7d37597d9013fcf0e987a5
Peterson Amblessed <amblessedpetercns@gmail.com>
2025-11-08 17:02:15 +0100

    added error handling and custom api error s and integrated it into my controller

--- controllers/task.js
+++ controllers/task.js
@@ -1,5 +1,6 @@
 const Task = require('../models/task');
 const asyncWrapper = require('../middleware/async-wrapper');
+const { createCustomError } = require('../errors/custom-errors');
 
 const getAllTasks = asyncWrapper( async (req, res) => {
   const tasks = await Task.find({});
@@ -15,31 +16,28 @@ const getTask = asyncWrapper(async (req, res, next) => {
   const {id:taskID} = req.params;
   const task = await Task.findOne({_id: taskID});
   if(!task){
-    const error = new Error('Task not found');
-    error.status = 404;
-    return next(error);
-    return res.status(404).json({msg: `No task with id : ${taskID}`})
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
   }
   res.status(200).json({ task });
 })
 
-const updateTask = async (req, res) => {
+const updateTask = asyncWrapper(async (req, res, next) => {
     const { id:taskID } = req.params;
     const task = await Task.findOneAndUpdate({_id: taskID}, req.body, {
       new: true,
       runValidators: true,
     })
     if (!task) {
-      return res.status(404).json({ msg: `No task with id : ${taskID}`})
+      return next(createCustomError(`No task with id : ${taskID}`, 404));
     }
-    res.status(201).json({ task })
-}
+    res.status(200).json({ task })
+})
 
-const deleteTask = asyncWrapper( async (req, res) => {
+const deleteTask = asyncWrapper( async (req, res, next) => {
   const {id:taskID} = req.params;
   const task = await Task.findOneAndDelete({_id: taskID});
   if(!task){
-    return res.status(404).json({msg: `No task with id : ${taskID}`})
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
   }
   res.status(200).json({ task });
 })
